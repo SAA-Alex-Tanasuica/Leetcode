5b944a56343867bb55c55d34adc682ec8b762712
Alexandru Tanasuica <dev.siralexaggressive@gmail.com>
2023-06-18 01:34:08 +0300

    Changes to solution

--- C#/1.Two Sum/1.TwoSum.cs
+++ C#/1.Two Sum/1.TwoSum.cs
@@ -1,15 +1,15 @@
-﻿public class Solution
-{
-    public int[] TwoSum(int[] nums, int target)
-    {
-        int index = 0;
-        for (int i = index + 1; i < nums.Length; i++)
+﻿public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for(int index = 0; index < nums.Length - 1; index++)
         {
-            if (nums[index] + nums[i] == target)
+            for(int i = index + 1; i < nums.Length; i++)
             {
-                return new[] { index, i };
+                if(nums[index] + nums[i] == target)
+                {
+                    Console.WriteLine($"{index},{i}");
+                    return new[] { index, i };
+                }
             }
-            index++;
         }
         return nums;
     }
